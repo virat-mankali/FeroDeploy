98874a3e98155d1933085a9d1a79ae072f938cfe
Virat Mankali <149859288+virat-mankali@users.noreply.github.com>
2025-01-13 20:09:59 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app, socketio
 
 # Create the app instance
@@ -5,4 +6,7 @@ app = create_app()
 
 # Run the app with Socket.IO
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    # Get the PORT from environment variables (default to 5000 if not set)
+    port = int(os.environ.get("PORT", 5000))
+    # Bind to all interfaces (0.0.0.0) and use the port
+    socketio.run(app, host="0.0.0.0", port=port, debug=True)
